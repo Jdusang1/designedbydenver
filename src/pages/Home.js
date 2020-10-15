a238a4493156b65526e77ab37046f77acff8cb2a
JohnDusang <Johndusang1@gmail.com>
2020-10-15 15:34:39 -0600

    changed jpg's to png's

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./Home.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Japan from "../assets/IMG_6917.JPG";
-import Thailand from "../assets/IMG_9025.png";
-import Nz from "../assets/IMG_3927.JPG"
-import { Card, CardImg } from 'reactstrap';
 
 function Home() {
     return (
@@ -15,42 +11,26 @@ function Home() {
             <div className="container-fluid">
                 <div className="row justify-content-around no-gutters">
                     <div className="col-4">
-                        <Card>
-                            <a href={"/about"}>
+                        <a href={"/about"}><div className="card-img fluid rounded-0" id="home-photo1">
+                            <h2>About</h2>
 
-                                <CardImg src={Japan} href={"/about"} className="card-img fluid rounded-0" id="home-photo1"></CardImg>
-                            </a>
-                        </Card>
+                        </div></a>
                     </div>
+
+
                     <div className="col-4">
-                        <Card>
-                            <a href={"/portfolio"}>
-                                <CardImg src={Thailand} href={"/about"} className="card-img fluid rounded-0" id="home-photo1"></CardImg>
-                            </a>
-                        </Card>
-                    </div>
-                    <div className="col-4">
-                        <Card>
-                            <a href={"/contact"}>
-                                <CardImg src={Nz} className="card-img fluid rounded-0" id="home-photo1"></CardImg>
-                            </a>
-                        </Card>
-                    </div>
-
-
-                    {/* <div className="col-4">
-                        <a href={"/portfolio"}> <div src={Thailand} className="card-img fluid rounded-0" id="home-photo2">
+                        <a href={"/portfolio"}> <div className="card-img fluid rounded-0" id="home-photo2">
                             <h2>Portfolio</h2>
 
                         </div></a>
                     </div>
                     <div className="col-4">
-                        <a href={"/contact"}> <div src={Nz} className="card-img fluid rounded-0" id="home-photo3">
+                        <a href={"/contact"}> <div className="card-img fluid rounded-0" id="home-photo3">
 
                             <h2>Contact</h2>
 
                         </div></a>
-                    </div> */}
+                    </div>
                 </div>
 
 
