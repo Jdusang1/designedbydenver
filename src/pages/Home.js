f8612ddc7fedb1a0f68f7c4030a361816d823e67
JohnDusang <Johndusang1@gmail.com>
2020-10-15 13:19:25 -0600

    troubleshooting photo issue fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Footer from "../components/Footer";
 import Japan from "../assets/IMG_6917.JPG";
 import Thailand from "../assets/IMG_9025.JPG";
 import Nz from "../assets/IMG_3927.JPG"
+import { Card, CardImg } from 'reactstrap';
 
 function Home() {
     return (
@@ -14,14 +15,29 @@ function Home() {
             <div className="container-fluid">
                 <div className="row justify-content-around no-gutters">
                     <div className="col-4">
-                        <a href={"/about"}><div src={Japan} className="card-img fluid rounded-0" id="home-photo1">
-                            <h2>About</h2>
-
-                        </div></a>
+                        <Card>
+                            <a href={"/about"}>
+                                <CardImg src={Japan} href={"/about"} className="card-img fluid rounded-0" id="home-photo1"></CardImg>
+                            </a>
+                        </Card>
+                    </div>
+                    <div className="col-4">
+                        <Card>
+                            <a href={"/portfolio"}>
+                                <CardImg src={Thailand} href={"/about"} className="card-img fluid rounded-0" id="home-photo1"></CardImg>
+                            </a>
+                        </Card>
+                    </div>
+                    <div className="col-4">
+                        <Card>
+                            <a href={"/contact"}>
+                                <CardImg src={Nz} className="card-img fluid rounded-0" id="home-photo1"></CardImg>
+                            </a>
+                        </Card>
                     </div>
 
 
-                    <div className="col-4">
+                    {/* <div className="col-4">
                         <a href={"/portfolio"}> <div src={Thailand} className="card-img fluid rounded-0" id="home-photo2">
                             <h2>Portfolio</h2>
 
@@ -33,7 +49,7 @@ function Home() {
                             <h2>Contact</h2>
 
                         </div></a>
-                    </div>
+                    </div> */}
                 </div>
 
 
