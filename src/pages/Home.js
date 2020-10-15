725482e727f0428fbefef86d5eae20f27b1cdbb9
JohnDusang <Johndusang1@gmail.com>
2020-10-15 13:06:37 -0600

    updating photos to hopefully work on live hosted site

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ function Home() {
             <div className="container-fluid">
                 <div className="row justify-content-around no-gutters">
                     <div className="col-4">
-                        <a href={"/about"}><div className="card-img fluid rounded-0" id="home-photo1">
+                        <a href={"/about"}><div src={Japan} className="card-img fluid rounded-0" id="home-photo1">
                             <h2>About</h2>
 
                         </div></a>
@@ -22,13 +22,13 @@ function Home() {
 
 
                     <div className="col-4">
-                        <a href={"/portfolio"}> <div className="card-img fluid rounded-0" id="home-photo2">
+                        <a href={"/portfolio"}> <div src={Thailand} className="card-img fluid rounded-0" id="home-photo2">
                             <h2>Portfolio</h2>
 
                         </div></a>
                     </div>
                     <div className="col-4">
-                        <a href={"/contact"}> <div className="card-img fluid rounded-0" id="home-photo3">
+                        <a href={"/contact"}> <div src={Nz} className="card-img fluid rounded-0" id="home-photo3">
 
                             <h2>Contact</h2>
 
