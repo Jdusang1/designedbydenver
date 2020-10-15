5af9c2132f9b09e2b78ec16810ecd775fa69ec55
JohnDusang <Johndusang1@gmail.com>
2020-10-15 13:01:51 -0600

    changed format of photos to hopefully load on live site

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ function Home() {
             <div className="container-fluid">
                 <div className="row justify-content-around no-gutters">
                     <div className="col-4">
-                        <a href={"/about"}><div img={Japan} className="card-img fluid rounded-0" id="home-photo1">
+                        <a href={"/about"}><div className="card-img fluid rounded-0" id="home-photo1">
                             <h2>About</h2>
 
                         </div></a>
@@ -22,13 +22,13 @@ function Home() {
 
 
                     <div className="col-4">
-                        <a href={"/portfolio"}> <div img={Thailand} className="card-img fluid rounded-0" id="home-photo2">
+                        <a href={"/portfolio"}> <div className="card-img fluid rounded-0" id="home-photo2">
                             <h2>Portfolio</h2>
 
                         </div></a>
                     </div>
                     <div className="col-4">
-                        <a href={"/contact"}> <div img={Nz} className="card-img fluid rounded-0" id="home-photo3">
+                        <a href={"/contact"}> <div className="card-img fluid rounded-0" id="home-photo3">
 
                             <h2>Contact</h2>
 
